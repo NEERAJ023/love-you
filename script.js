4a67dbb94d79ace6100347f8046a4dc4927d79ce
neeraj023 <neeraj80558055@gmail.com>
2024-01-12 14:24:44 +0530

    ji

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 let h1=document.querySelector(".h1");
+let btn= document.querySelectorAll(".btn");
 function clr(){
     for(let i=0; i<btn.length; i++){
         btn[i].remove();
     }
-h1.innerHTML="Love u...💛";
+h1.innerHTML="Mtlb chance hai ...💛";
 }
-let btn= document.querySelectorAll(".btn");
+
+
+let no=document.querySelector("#no");
+let card=document.querySelector(".card");
+let nobtn=no.getBoundingClientRect();
+let carddiv=card.getBoundingClientRect();
+console.log(nobtn);
+
+no.addEventListener("mouseover",()=>{
+    let i= Math.floor(Math.random()*(carddiv.width-nobtn.width))+3;
+    let j= Math.floor(Math.random()*(carddiv.height-nobtn.height))+3;
+    no.style.top= j+'px';
+    no.style.left= i+'px';
+})
